3c5a447f843dba787d28e53066e3c8e76c0a6bf5
Micah Read <164657374+P5YC0DR3AM3R@users.noreply.github.com>
2024-03-24 16:09:41 -0400

    Debugged JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,27 @@
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for(let x = 0; x < topics.length; x++)
-{
-    console.log(topics[x]);
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics() {
+    for (let x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
    }
-/*
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
 }
-*/
+
+function selectTopic(topic) {
+    if (topic === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (topic === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (topic === 'Git') {
+      console.log("Let's study Git!");
+    } else if (topic === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
+}
+
+console.log('Here are the topics we learned through Prewor:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic(randomTopic);
